9bcbe0d398b16282581cf886777482350af56d92
Arjun Mahesh <arjun.mahesh@duke.edu>
2023-03-26 18:29:30 -0400

    algo changes

--- python-algo/algo_strategy.py
+++ python-algo/algo_strategy.py
@@ -79,26 +79,40 @@ class AlgoStrategy(gamelib.AlgoCore):
         self.build_reactive_defense(game_state)
 
         # If the turn is less than 5, stall with interceptors and wait to see enemy's base
+        #TODO Why less than 5? Let's return to this
         if game_state.turn_number < 5:
             self.stall_with_interceptors(game_state)
         else:
+            
             # Now let's analyze the enemy base to see where their defenses are concentrated.
             # If they have many units in the front we can build a line for our demolishers to attack them at long range.
             if self.detect_enemy_unit(game_state, unit_type=None, valid_x=None, valid_y=[14, 15]) > 10:
                 self.demolisher_line_strategy(game_state)
             else:
                 # They don't have many units in the front so lets figure out their least defended area and send Scouts there.
-
-                # Only spawn Scouts every other turn
+                #TODO Is this optimal strategy?
+                
+                # Only spawn Scouts every every other turn
                 # Sending more at once is better since attacks can only hit a single scout at a time
-                if game_state.turn_number % 2 == 1:
+                if game_state.turn_number % 3 == 1:
                     # To simplify we will just check sending them from back left and right
-                    scout_spawn_location_options = [[13, 0], [14, 0]]
+
+
+                    #Added more places on the board
+                    scout_spawn_location_options = []
+                    for x in range(14):
+                        scout_spawn_location_options.append([x,13 -x])
+                    for x in range(14, 28):
+                        scout_spawn_location_options.append([x, 27 - x])
+
+                    
                     best_location = self.least_damage_spawn_location(game_state, scout_spawn_location_options)
                     game_state.attempt_spawn(SCOUT, best_location, 1000)
 
                 # Lastly, if we have spare SP, let's build some supports
-                support_locations = [[13, 2], [14, 2], [13, 3], [14, 3]]
+                #TODO change to game specifications
+                support_locations = [[5, 13], [11, 13], [17, 13], [22, 13]]
+                #TODO Why these locations?
                 game_state.attempt_spawn(SUPPORT, support_locations)
 
     def build_defences(self, game_state):
@@ -119,6 +133,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         game_state.attempt_spawn(WALL, wall_locations)
         # upgrade walls so they soak more damage
         game_state.attempt_upgrade(wall_locations)
+ 
 
     def build_reactive_defense(self, game_state):
         """
@@ -135,24 +150,28 @@ class AlgoStrategy(gamelib.AlgoCore):
         """
         Send out interceptors at random locations to defend our base from enemy moving units.
         """
+        #TODO Why random? Use reactive ex. methods above
         # We can spawn moving units on our edges so a list of all our edge locations
         friendly_edges = game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_LEFT) + game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_RIGHT)
         
         # Remove locations that are blocked by our own structures 
         # since we can't deploy units there.
         deploy_locations = self.filter_blocked_locations(friendly_edges, game_state)
-        
+        best_location = self.least_damage_spawn_location(game_state, deploy_locations)
         # While we have remaining MP to spend lets send out interceptors randomly.
-        while game_state.get_resource(MP) >= game_state.type_cost(INTERCEPTOR)[MP] and len(deploy_locations) > 0:
-            # Choose a random deploy location.
-            deploy_index = random.randint(0, len(deploy_locations) - 1)
-            deploy_location = deploy_locations[deploy_index]
+        while game_state.get_resource(MP) >= game_state.type_cost(INTERCEPTOR)[MP] and len(best_location) > 0:
+            ## Choose a random deploy location.
+            #deploy_index = random.randint(0, len(deploy_locations) - 1)
+            #deploy_location = deploy_locations[deploy_index]
             
-            game_state.attempt_spawn(INTERCEPTOR, deploy_location)
+            game_state.attempt_spawn(INTERCEPTOR, best_location)
             """
             We don't have to remove the location since multiple mobile 
             units can occupy the same space.
             """
+
+
+
 
     def demolisher_line_strategy(self, game_state):
         """
